79fb3f388b85feeded9c76ac61c2a6ceabf1e7ec
TranBaSon <sontbth1808021@fpt.edu.vn>
2019-09-25 20:13:10 +0700

    push

--- app/Http/Controllers/feedbackController.php
+++ app/Http/Controllers/feedbackController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\studentFeedback;
 use Illuminate\Http\Request;
 
+
 class feedbackController extends Controller
 {
     public function surveyPost(Request $request){
@@ -13,7 +14,8 @@ class feedbackController extends Controller
         request()->validate([
             'name' => 'required',
             'email' => 'required|email|unique:student_feedback',
-            'telephone' => 'required|unique:student_feedback'
+            'telephone' => 'required',
+            'feedback' => 'required'
             ]);
              
             $data = request()->except(['_token']);
@@ -26,35 +28,6 @@ class feedbackController extends Controller
             return  Response()->json($arr);
 
 
-
-
-        // $messages = [
-        //     "required" => "Bắt buộc phải nhập thông tin",
-        //     "string"    => "Phải nhập vào 1 chuỗi",
-        //     "numeric"   => "Phải nhập vào 1 số",
-        //     "min"   => "Giá trị tối 6 ký tự",
-        //     "max"   => "Tối đa 255 ký tự",
-        //     "unique" => "Đã tồn tại giá trị này"
-        // ];
-        // $rules = [
-        //     "name" => "required|string|max:255",
-        //     "email"   => "required|email",
-        //     "telephone"=> "required|numeric|min:10",
-        //     "feedback"=>"required"
-        // ];
-        // $this->validate($request,$rules,$messages);
-
-        // try{
-        //     studentFeedback::create([
-        //         "name"=> $request->get("name"),
-        //         "email"=> $request->get("email"),
-        //         "telephone"=> $request->get("telephone"),
-        //         "feedback"=> $request->get("feedback"),
-        //     ])->save();
-        // }catch (\Exception $e){
-        //     die($e->getMessage());
-        // }
-        // return redirect("survey");
     }
 
 }
